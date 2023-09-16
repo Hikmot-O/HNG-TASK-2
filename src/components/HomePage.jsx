17711cc2f779ec32f9c71f1d3a16c3ecc17bf7be
Hikmot-O <hikmotokunola@gmail.com>
2023-09-16 23:17:03 +0100

    fix

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Hero from "./Hero";
 import TopMovies from "./TopMovies";
 // import Footer from "./footer";
-import Footer from "./footer";
+import Footer from "./Footer";
 
 const HomePage = () => {
   return <section className="h-screen overflow-y-auto">
